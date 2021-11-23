23222c6f4364d849e1ec3945461fb6cc984bf65a
Zair <nurmukhamadov.z@future-group.ru>
2021-11-23 17:23:04 +0300

    refactoring & publish tag

--- src/LaraAdminServiceProvider.php
+++ src/LaraAdminServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace Future\LaraAdmin;
 
-use Future\LaraAdmin\Commands\MainCommand;
 use Future\LaraAdmin\Commands\UiCommand;
 use Future\LaraAdmin\View\Components\Sidebar;
 use Future\LaraAdmin\View\Components\Menu;
@@ -12,22 +11,24 @@ class LaraAdminServiceProvider extends ServiceProvider
 {
 	public function boot()
 	{
-		if ($this->app->runningInConsole()) {
-			$this->commands([
-				MainCommand::class,
-//				AdminCommand::class,
-//				AuthCommand::class,
-				UiCommand::class
-			]);
-		}
-
+		$this->registerCommands();
 		$this->registerViews();
 		$this->registerViewComponents();
+		$this->registerPublished();
 	}
 
 	public function register()
 	{
 		//
+	}
+
+	protected function registerCommands(): void
+	{
+		if ($this->app->runningInConsole()) {
+			$this->commands([
+				UiCommand::class
+			]);
+		}
 	}
 
 	protected function registerViews(): void
@@ -41,5 +42,12 @@ class LaraAdminServiceProvider extends ServiceProvider
 			Sidebar::class,
 			Menu::class,
 		]);
+	}
+
+	protected function registerPublished(): void
+	{
+		$this->publishes([
+			__DIR__.'/../resources/views' => resource_path('views/vendor/courier'),
+		], 'views');
 	}
 }
