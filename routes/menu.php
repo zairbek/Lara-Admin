6894750106b5bb7edf2745fce9c4f230dec29b9d
Zair <nurmukhamadov.z@future-group.ru>
2021-12-06 17:32:31 +0300

    fix

--- routes/menu.php
+++ routes/menu.php
@@ -6,13 +6,13 @@ use Spatie\Menu\Laravel\Html;
 Menu::macro('main', function () {
     return Menu::mainMenu()
         ->addIfCan(['admin@show'], Html::mainMenuItem('/admin', 'Dashboard', ['icon' => 'fas fa-tachometer-alt']))
-        ->add(Html::mainMenuEmptyItem('Настройки'))
+        ->addIfCan(['admin@show'], Html::mainMenuEmptyItem('Настройки'))
         ->customSubmenu(
             'Пользователи',
             Menu::tree()
-            ->add(Html::mainMenuItem('/admin/settings/users', 'Пользователи', ['icon' => 'fas fa-user']))
-            ->add(Html::mainMenuItem('/admin/settings/roles', 'Группы', ['icon' => 'fas fa-users']))
-            ->add(Html::mainMenuItem('/admin/settings/permissions', 'Доступы', ['icon' => 'fas fa-user-lock']))
+            ->addIfCan(['users@show'], Html::mainMenuItem('/admin/settings/users', 'Пользователи', ['icon' => 'fas fa-user']))
+            ->addIfCan(['roles@show'], Html::mainMenuItem('/admin/settings/roles', 'Группы', ['icon' => 'fas fa-users']))
+            ->addIfCan(['permissions@show'], Html::mainMenuItem('/admin/settings/permissions', 'Доступы', ['icon' => 'fas fa-user-lock']))
         )
     ;
 });
