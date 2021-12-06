a864edf70341f5c238dd0fedda76c093567d0759
Zair <nurmukhamadov.z@future-group.ru>
2021-12-06 12:20:11 +0300

    preloader

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -11,10 +11,12 @@
 <div class="wrapper">
 
 
+    {{--
     <!-- Preloader -->
     <div class="preloader flex-column justify-content-center align-items-center">
         <img class="animation__wobble" src="{{ asset('dist/img/AdminLTELogo.png') }}" alt="AdminLTELogo" height="60" width="60">
     </div>
+    --}}
 
     <!-- Navbar -->
     <nav class="main-header navbar navbar-expand navbar-dark">
